62177a6fd03f7c88a3540361f58c100e224a7647
tmyknmd <thais.nomada@gmail.com>
2026-06-18 09:41:10 -0300

    REFACTOR: estrutura do sistema.html

--- prototipo/telas/js/acessibilidade.js
+++ prototipo/telas/js/acessibilidade.js
@@ -4,6 +4,8 @@ var skin = false;
 function skin_alto_contraste() {
     if (!skin){
         r.style.setProperty('--cinza-escuro', '#000');
+        r.style.setProperty('--cinza-login', '#000');
+        r.style.setProperty('--cinza-login-hover', '#333');
         r.style.setProperty('--branco', '#000');
         r.style.setProperty('--azul', '#333');
         r.style.setProperty('--azul-claro', '#ff6');
@@ -14,10 +16,14 @@ function skin_alto_contraste() {
 
         r.style.setProperty('--sist-borda-cinza', '#fff');
         r.style.setProperty('--sist-preench-cinza', '#000');
+        r.style.setProperty('--sist-borda-amarela', '#fff');
+        r.style.setProperty('--sist-preench-amarelo', '#000');
         r.style.setProperty('--sist-preench-verm', '#000');
         r.style.setProperty('--sist-txt-cinza-escuro', '#fff');
     }else{
         r.style.setProperty('--cinza-escuro', '#3f4e55');
+        r.style.setProperty('--cinza-login', '#576870');
+        r.style.setProperty('--cinza-login-hover', '#879398');
         r.style.setProperty('--branco', '#fff');
         r.style.setProperty('--azul', '#0072bb');
         r.style.setProperty('--azul-claro', '#0087de');
@@ -28,6 +34,8 @@ function skin_alto_contraste() {
 
         r.style.setProperty('--sist-borda-cinza', '#ddd');
         r.style.setProperty('--sist-preench-cinza', '#eee');
+        r.style.setProperty('--sist-borda-amarela', '#e6d59b');
+        r.style.setProperty('--sist-preench-amarelo', '#fff4c8');
         r.style.setProperty('--sist-preench-verm', '#fdd');
         r.style.setProperty('--sist-txt-cinza-escuro', '#888');
     }
